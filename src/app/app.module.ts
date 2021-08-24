cd48aaf66c8532513d1786e10dd83acf57ae6399
Karoline Toledo <karoltoledo71@hotmail.com>
2021-08-24 12:23:55 -0300

    Implementação do Menu e Rodapé do Blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntrarComponent,
+    CadastrarComponent,
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule
