bef1d1dcb5e18318bc49e4d97747094c5b54066d
Karoline Toledo <karoltoledo71@hotmail.com>
2021-08-25 20:52:16 -0300

    Implementando rotas nas páginas do blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 
+const routes: Routes = [
+  {path:"", redirectTo: "entrar", pathMatch:"full"},
+
+  {path:"entrar", component: EntrarComponent},
+  {path:"cadastrar", component: CadastrarComponent}
+];
+
 @NgModule({
+  
   declarations: [
     AppComponent,
     EntrarComponent,
@@ -16,8 +25,11 @@ import { RodapeComponent } from './rodape/rodape.component';
     RodapeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
+  exports: [RouterModule],
+
   providers: [],
   bootstrap: [AppComponent]
 })
